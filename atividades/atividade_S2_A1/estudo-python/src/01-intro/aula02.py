163ac2d79f10e5ebc3e03445284cd91e500096f4
adalbertobrant <adalbertobrant@gmail.com>
2025-12-10 08:54:09 -0300

    aula 2 de python

---
+++ atividades/atividade_S2_A1/estudo-python/src/01-intro/aula02.py
@@ -0,0 +1,10 @@
+""" Aula 02  - Keywords e identificadores """
+
+MAIORIDADE = 18
+
+idade = int(input('Entre a idade -> '))
+
+if idade >= 18:
+    print('Maior de idade')
+else:
+    print('Menor de idade')
